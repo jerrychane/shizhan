6e0b87bc04cbcd05ce119d1b9c524acc04047fb5
jerrychane <jerrychane@126.com>
2021-01-23 16:47:13 +0800

    TS+React组件库：函数和类型判断

---
+++ learn-ts/functions.ts
@@ -0,0 +1,19 @@
+// 函数声明
+// function add(x: number, y: number, z: number = 10): number {
+//     if (typeof z === 'number') {
+//         return x + y + z
+//     } else {
+//         return x + y
+//     }
+// }
+// let result = add(2, 3)
+
+// 函数表达式,类型推断
+const add = function (x: number, y: number, z: number = 10): number {
+    if (typeof z === 'number') {
+        return x + y + z
+    } else {
+        return x + y
+    }
+}
+const add2: (x: number, y: number, z?: number) => number = add 
