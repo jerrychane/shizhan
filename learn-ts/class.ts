50caac8785733daa186ce13c804a2bdb40d1141a
jerrychane <jerrychane@126.com>
2021-01-24 21:43:21 +0800

    TS+React组件库：类和接口

--- learn-ts/class.ts
+++ learn-ts/class.ts
@@ -41,3 +41,26 @@ class Cat extends Animal {
 }
 const maomao = new Cat('maomao')
 console.log(maomao.run())
+
+interface Radio {
+    switchRadio(): void;
+}
+interface Battery {
+    checkBatteryStatus();
+}
+interface RadioWithBattery extends Radio {
+    checkBatteryStatus();
+}
+class Car implements Radio {
+    switchRadio() {
+
+    }
+}
+class Cellphone implements RadioWithBattery {
+    switchRadio() {
+
+    }
+    checkBatteryStatus() {
+
+    }
+}
