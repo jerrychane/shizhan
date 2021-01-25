3ac7388fb9007e2164ab6578c757ff9b8686e37b
jerrychane <jerrychane@126.com>
2021-01-25 21:57:45 +0800

    泛型-约束泛型

--- learn-ts/generics.ts
+++ learn-ts/generics.ts
@@ -7,4 +7,21 @@ const result = echo(123)
 function swap<T, U>(tuple: [T, U]): [U, T] {
     return [tuple[1], tuple[0]]
 }
-const result2 = swap(['string', 123]) 
+const result2 = swap(['string', 123])
+// 约束泛型-方案一,返回结果只能是数组
+function echoWithArr<T>(arg: T[]): T[] {
+    console.log(arg.length)
+    return arg
+}
+const arrs = echoWithArr([1, 2, 3])
+// 约束泛型-方案二,通过 extends 和 interface 设置约束条件
+interface IWithLength {
+    length: number;
+}
+function echoWithLength<T extends IWithLength>(arg: T): T {
+    console.log(arg.length)
+    return arg
+}
+const str = echoWithLength('str')
+const obj = echoWithLength({ length: 10, width: 10 })
+const arr2 = echoWithLength([1, 2, 3])
