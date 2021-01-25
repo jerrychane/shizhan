b29b54551113b4803ea9a38c929cf7f787ec1d12
jerrychane <jerrychane@126.com>
2021-01-25 22:18:42 +0800

    TS+React组件库：泛型-类和接口

--- learn-ts/generics.ts
+++ learn-ts/generics.ts
@@ -25,3 +25,44 @@ function echoWithLength<T extends IWithLength>(arg: T): T {
 const str = echoWithLength('str')
 const obj = echoWithLength({ length: 10, width: 10 })
 const arr2 = echoWithLength([1, 2, 3])
+// 类使用泛型约束
+class Queue<T> {
+    private data = []
+    push(item: T) {
+        return this.data.push(item)
+    }
+    pop(): T {
+        return this.data.shift()
+    }
+}
+const queque = new Queue<number>()
+queque.push(1)
+console.log(queque.pop().toFixed())
+
+const queque2 = new Queue<string>()
+queque2.push('str')
+console.log(queque2.pop().length)
+
+// 接口添加泛型
+interface KeyPair<T, U> {
+    key: T;
+    value: U;
+}
+let kp1: KeyPair<number, string> = { key: 123, value: 'str' }
+let kp2: KeyPair<string, number> = { key: 'test', value: 123 }
+
+let arr: number[] = [1, 2, 3]
+let arrTwo: Array<number> = [1, 2, 3]
+
+// inerface 描述一个函数的type
+interface IPlus<T> {
+    (a: T, b: T): T
+}
+function plus(a: number, b: number): number {
+    return a + b;
+}
+function connect(a: string, b: string) {
+    return a + b;
+}
+const a: IPlus<number> = plus
+const b: IPlus<string> = connect
