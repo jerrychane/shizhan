c6aaf0532fe8aeeea88b7f6da9b2fbdb1d972f37
jerrychane <jerrychane@126.com>
2021-02-05 22:20:05 +0800

    在页面中显示获取到的设备

--- webrtclearning/device/js/client.js
+++ webrtclearning/device/js/client.js
@@ -1,4 +1,8 @@
 "use strict";
+var audioSource = document.querySelector("select#audioSource");
+var audioOutput = document.querySelector("select#audioOutput");
+var videoSource = document.querySelector("select#videoSource");
+
 if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
   console.log("enumerateDevices is not supported!");
 } else {
@@ -18,6 +22,16 @@ function gotDevice(deviceInfos) {
         ":groupId = " +
         deviceInfo.groupId
     );
+    var option = document.createElement("option");
+    option.text = deviceInfo.label;
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === "audioinput") {
+      audioSource.appendChild(option);
+    } else if (deviceInfo.kind === "audiooutput") {
+      audioOutput.appendChild(option);
+    } else if (deviceInfo.kind === "videoinput") {
+      videoSource.appendChild(option);
+    }
   });
 }
 
