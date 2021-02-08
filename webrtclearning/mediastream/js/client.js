1370990398d2d7b476658efbe13eb6b188bff667
jerrychane <jerrychane@126.com>
2021-02-08 23:49:56 +0800

    安全管理-获取音视频设备的访问权限

--- webrtclearning/mediastream/js/client.js
+++ webrtclearning/mediastream/js/client.js
@@ -1,8 +1,27 @@
 "use strict";
 var videoplay = document.querySelector("video#player");
+let audioSource = document.querySelector("select#audioSource");
+let audioOutput = document.querySelector("select#audioOutput");
+let videoSource = document.querySelector("select#videoSource");
+
+function gotDevices(deviceInfos) {
+  deviceInfos.forEach((deviceInfo) => {
+    let option = document.createElement("option");
+    option.text = deviceInfo.label;
+    option.value = deviceInfo.deviceId;
+    if (deviceInfo.kind === "audioinput") {
+      audioSource.appendChild(option);
+    } else if (deviceInfo.kind === "audiooutput") {
+      audioOutput.appendChild(option);
+    } else if (deviceInfo.kind === "videoinput") {
+      videoSource.appendChild(option);
+    }
+  });
+}
 
 function gotMediaStream(stream) {
   videoplay.srcObject = stream;
+  return navigator.mediaDevices.enumerateDevices();
 }
 
 function handleError(error) {
@@ -19,5 +38,6 @@ if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
   navigator.mediaDevices
     .getUserMedia(constraints)
     .then(gotMediaStream)
+    .then(gotDevices)
     .catch(handleError);
 }
