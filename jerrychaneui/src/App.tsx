540555d110f4dfb4cabfad8dfc7699cc44876383
jerrychane <jerrychane@126.com>
2021-02-17 22:10:45 +0800

    add normalize.scss

--- jerrychaneui/src/App.tsx
+++ jerrychaneui/src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   const a = '123'
@@ -10,18 +8,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello world</h1>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+        <hr />
+        <code>
+          const a = 'b'
+        </code>
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload
+         </p>
       </header>
     </div>
   );
