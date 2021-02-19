e3f4ee80b4890738df91a2a9c6a0f0a0e6003eec
jerrychane <jerrychane@126.com>
2021-02-19 22:21:23 +0800

    精益求精 - Buton 组件编码第二部分

--- jerrychaneui/src/components/Button/button.tsx
+++ jerrychaneui/src/components/Button/button.tsx
@@ -21,17 +21,25 @@ interface BaseButtonProps {
     children?: React.ReactNode,
     href?: string,
 }
+// 获取按钮上的属性，并合并
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+// 获取链接上的属性，并合并
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+// 合并所有属性，并导出
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
-const Button: React.FC<BaseButtonProps> = (props) => {
+const Button: React.FC<ButtonProps> = (props) => {
     const {
         disabled,
         size,
+        className,
         btnType,
         children,
-        href
+        href,
+        ...resProps
     } = props
     // btn,btn-lg,btn-primary
-    const classes = classNames('btn', {
+    const classes = classNames('btn', className, {
         [`btn-${btnType}`]: btnType,
         [`btn-${size}`]: size,
         'disabled': (btnType === ButtonType.Link) && disabled,
@@ -41,6 +49,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
             <a
                 className={classes}
                 href={href}
+                {...resProps}
             >
                 {children}
             </a>
@@ -50,6 +59,7 @@ const Button: React.FC<BaseButtonProps> = (props) => {
             <button
                 className={classes}
                 disabled={disabled}
+                {...resProps}
             >
                 {children}
             </button>
