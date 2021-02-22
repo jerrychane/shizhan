89949b39d614ba9e62aa154b56bf19b8e61d4a54
jerrychane <jerrychane@126.com>
2021-02-22 22:30:31 +0800

    基础架构 - Menu组件编码第一部分

--- jerrychaneui/src/App.tsx
+++ jerrychaneui/src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+
 function App() {
   const a = '123'
   if (a === '123') {
@@ -8,6 +11,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem> cool link 1</MenuItem>
+          <MenuItem> cool link 2</MenuItem>
+          <MenuItem> cool link 3</MenuItem>
+        </Menu>
         <Button autoFocus onClick={(e) => { e.preventDefault(); alert(123) }} className="custom"> Hello </Button>
         <Button disabled> Disabled Button </Button>
         <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Large Primary </Button>
