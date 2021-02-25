f58d3abd433bee7cd9e1c9c08294257fb26c11e3
jerrychane <jerrychane@126.com>
2021-02-25 23:22:11 +0800

    去掉index参数

--- jerrychaneui/src/components/Menu/menu.tsx
+++ jerrychaneui/src/components/Menu/menu.tsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from 'react'
 import classNames from 'classnames'
+import MenuItem, { MenuItemProps } from './menuItem'
 
 type MenuMode = 'horizontal' | 'vertical'
 type SelectCallback = (selectedIndex: number) => void
@@ -33,10 +34,21 @@ const Menu: React.FC<MenuProps> = (props) => {
         index: currentActive ? currentActive : 0,
         onSelect: handleClick,
     }
+    const renderChildren = () => {
+        return React.Children.map(children, (child, index) => {
+            const childElement = child as React.FunctionComponentElement<MenuItemProps>
+            const { displayName } = childElement.type
+            if (displayName === "MenuItem") {
+                return React.cloneElement(childElement, { index })
+            } else {
+                console.error('Warning: Menu has a child which is not a MenuItem component!')
+            }
+        })
+    }
     return (
         <ul className={classes} style={style} data-testid="test-menu">
             <MenuContext.Provider value={passContext}>
-                {children}
+                {renderChildren()}
             </MenuContext.Provider>
         </ul>
     )
@@ -46,5 +58,4 @@ Menu.defaultProps = {
     defaultIndex: 0,
     mode: 'horizontal'
 }
-
 export default Menu;
