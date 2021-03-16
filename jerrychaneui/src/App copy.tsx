57ac9ae103dadbfcb6efd2bd4f523bacff0f673e
jerrychane <jerrychane@126.com>
2021-03-16 22:50:44 +0800

    10-3 test axios with JsonPlaceHolder

---
+++ jerrychaneui/src/App copy.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
+import SubMenu from './components/Menu/subMenu'
+import Icon from './components/Icon/icon'
+import Input from './components/Input/input'
+import Transition from './components/Transition/transition'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+function App() {
+  const a = '123'
+  if (a === '123') {
+
+  }
+  const [show, setShow] = useState(false)
+  return (
+    <div className="App">
+      <header className="App-header">
+        <FontAwesomeIcon icon={"coffee"} size='10x' />
+        <Icon icon="arrow-down" theme="primary" size="10x" />
+        <Menu
+          defaultIndex="0"
+          onSelect={(index) => { alert(index) }}
+          mode={'horizontal'}
+          // mode={'vertical'}
+          defaultOpenSubMenus={['2']}
+        >
+          <MenuItem > cool link 0</MenuItem>
+          <MenuItem disabled> cool link 1</MenuItem>
+          <SubMenu title='dropdown'>
+            <MenuItem>dropdown 1</MenuItem>
+            <MenuItem>dropdown 2</MenuItem>
+          </SubMenu>
+          <MenuItem > cool link 2</MenuItem>
+        </Menu>
+        <Button size="sm" onClick={() => { setShow(!show) }}>Toggle</Button>
+        <Transition
+          in={show}
+          timeout={300}
+          animation="zoom-in-left"
+        >
+          <div>
+            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
+            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
+            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
+            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
+            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
+          </div>
+        </Transition>
+        <Transition
+          in={show}
+          timeout={300}
+          animation="zoom-in-top"
+          wrapper
+        >
+          <Button btnType="primary" size='lg'>A Large Button</Button>
+        </Transition>
+        <p> learn react </p>
+        <div style={{ width: "33.33%" }}>
+
+          <Input disabled />
+          <Input size="sm" />
+          <Input size="lg" />
+          <Input prepend="http://" />
+          <Input append=".com" />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- jerrychaneui/src/App.tsx
+++ jerrychaneui/src/App.tsx
@@ -1,76 +1,34 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
-import Menu from './components/Menu/menu'
-import MenuItem from './components/Menu/menuItem'
-import SubMenu from './components/Menu/subMenu'
-import Icon from './components/Icon/icon'
-import Input from './components/Input/input'
-import Transition from './components/Transition/transition'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-library.add(fas)
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
-function App() {
-  const a = '123'
-  if (a === '123') {
-
+// theFileYouDeclaredTheCustomConfigIn.ts
+declare module 'axios' {
+  export interface AxiosRequestConfig {
+    handlerEnabled: boolean;
   }
-  const [show, setShow] = useState(false)
+}
+
+const App: React.FC = () => {
+  const [title, setTitle] = useState('')
+  const postData = {
+    title: 'my title',
+    body: 'hello man'
+  }
+  useEffect(() => {
+    axios.post('http://jsonplaceholder.typicode.com/posts', postData).then(
+      resp => {
+        console.log('resp', resp)
+        setTitle(resp.data.title)
+      }
+    )
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <FontAwesomeIcon icon={"coffee"} size='10x' />
-        <Icon icon="arrow-down" theme="primary" size="10x" />
-        <Menu
-          defaultIndex="0"
-          onSelect={(index) => { alert(index) }}
-          mode={'horizontal'}
-          // mode={'vertical'}
-          defaultOpenSubMenus={['2']}
-        >
-          <MenuItem > cool link 0</MenuItem>
-          <MenuItem disabled> cool link 1</MenuItem>
-          <SubMenu title='dropdown'>
-            <MenuItem>dropdown 1</MenuItem>
-            <MenuItem>dropdown 2</MenuItem>
-          </SubMenu>
-          <MenuItem > cool link 2</MenuItem>
-        </Menu>
-        <Button size="sm" onClick={() => { setShow(!show) }}>Toggle</Button>
-        <Transition
-          in={show}
-          timeout={300}
-          animation="zoom-in-left"
-        >
-          <div>
-            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
-            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
-            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
-            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
-            <p> Edit <code>src/App.tsx</code> and save to reload.</p>
-          </div>
-        </Transition>
-        <Transition
-          in={show}
-          timeout={300}
-          animation="zoom-in-top"
-          wrapper
-        >
-          <Button btnType="primary" size='lg'>A Large Button</Button>
-        </Transition>
-        <p> learn react </p>
-        <div style={{ width: "33.33%" }}>
-
-          <Input disabled />
-          <Input size="sm" />
-          <Input size="lg" />
-          <Input prepend="http://" />
-          <Input append=".com" />
-        </div>
+        <h1>{title}</h1>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
