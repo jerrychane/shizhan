09058b08b3335be8fa62c183f9d997941c73e1bb
jerrychane <jerrychane@126.com>
2021-03-17 21:33:26 +0800

    three method of file to upload

---
+++ jerrychaneui/src/App copy 2.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const App: React.FC = () => {
+  const [title, setTitle] = useState('')
+  const postData = {
+    title: 'my title',
+    body: 'hello man'
+  }
+  useEffect(() => {
+    axios.post('http://jsonplaceholder.typicode.com/posts', postData).then(
+      resp => {
+        console.log('resp', resp)
+        setTitle(resp.data.title)
+      }
+    )
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{title}</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App;
--- jerrychaneui/src/App.tsx
+++ jerrychaneui/src/App.tsx
@@ -1,32 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
 
-// theFileYouDeclaredTheCustomConfigIn.ts
-declare module 'axios' {
-  export interface AxiosRequestConfig {
-    handlerEnabled: boolean;
-  }
-}
-
 const App: React.FC = () => {
-  const [title, setTitle] = useState('')
-  const postData = {
-    title: 'my title',
-    body: 'hello man'
-  }
-  useEffect(() => {
-    axios.post('http://jsonplaceholder.typicode.com/posts', postData).then(
-      resp => {
-        console.log('resp', resp)
-        setTitle(resp.data.title)
-      }
-    )
-  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>{title}</h1>
-      </header>
+    <div className="App" style={{ marginTop: '100px', marginLeft: '100px' }}>
+      <form method="post" encType="multipart/form-data" action="http://jsonplaceholder.typicode.com/posts">
+        <input type="file" name="myFile" />
+        <button type="submit">Submit</button>
+      </form>
+
     </div>
   )
 }
