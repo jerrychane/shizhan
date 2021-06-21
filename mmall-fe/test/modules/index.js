7c3c16b38c9f3aeb43eea1754e420ef42dce58ff
jerrychane <jerrychane@126.com>
2021-06-21 21:44:27 +0800

    2021年06月20日 周一 26周 2-4 ES6基础（2）

---
+++ mmall-fe/test/modules/index.js
@@ -0,0 +1,7 @@
+import { str, obj, fn } from "./module.js";
+import foo from './module.js'
+
+console.log('str:', str);
+console.log('name:', obj.name);
+console.log('fn:', fn);
+console.log('foo:', foo);
---
+++ mmall-fe/test/modules/module.js
@@ -0,0 +1,13 @@
+let str = 'string';
+let obj = {
+    name: "Rosen"
+};
+let fn = () => {
+    console.log('module test')
+};
+export {
+    str,
+    obj,
+    fn
+}
+export default { a: 1 }
